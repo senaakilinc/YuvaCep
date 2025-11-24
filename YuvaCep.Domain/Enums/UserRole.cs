02b72f9c8f89eb84be11ec6a094c9a183836ed2d
senaakilinc <sena.kilinc@outlook.com.tr>
2025-11-24 22:24:05 +0300

    added domains structures

---
+++ YuvaCep.Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuvaCep.Domain.Enums
+{
+    public enum UserRole
+    {
+        Parent = 1,
+        Teacher = 2,
+        Admin = 3 
+    }
+}
