c4826505dbc0bf43b83c59e10b582999af3363b4
nedasaygili <nedasaygili@gmail.com>
2025-12-27 21:06:49 +0300

    deleted admin from UserRole

--- YuvaCep.Domain/Entities/User.cs
+++ YuvaCep.Domain/Entities/User.cs
@@ -8,7 +8,7 @@ namespace YuvaCep.Domain.Entities
         public Guid Id { get; set; } // Kimlik No
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-        public UserRole Role { get; set; } // Admin mi, Öğretmen mi?
+        public UserRole Role { get; set; } //  Öğretmen mi?
         public string? TCIDNumber { get; set; }
 
         // Bu kullanıcı bir Öğretmen ise onun profilini tutar
@@ -16,8 +16,8 @@ namespace YuvaCep.Domain.Entities
 
         // Bu kullanıcı bir Veli ise onun profilini tutar
         public Parent? ParentProfile { get; set; }
-        public string PasswordSalt { get; set; }
-        public string ReferenceCode { get; set; }
+        public required string PasswordSalt { get; set; }
+        public required string ReferenceCode { get; set; }
         public bool IsActive { get; set; }
     }
 }
