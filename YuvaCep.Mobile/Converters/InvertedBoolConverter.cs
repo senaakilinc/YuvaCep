ef5ec4baad2c295e7ac3ba21fab01875164e1f79
eminbadur <baduremin675@icloud.com>
2026-01-06 05:31:59 +0300

    Added gamification with puzzle system

---
+++ YuvaCep.Mobile/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace YuvaCep.Mobile.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
